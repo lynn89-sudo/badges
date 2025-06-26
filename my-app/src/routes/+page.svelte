a72c613897b109863917e531cd3ec84752f31d6c
lynn89-sudo <luke87joseph@gmail.com>
2025-06-26 03:41:29 +0000

    Added in basic navbar

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,6 +1,27 @@
 <script>
-    let logo_src = "null"
     import flag from "$lib/assets/orpheus.svg";
+    import { onMount } from "svelte";
+    //import icon from "$lib/assets/icon.svg";
+    let icon = "";
+    let icon_load = false;
+
+    let nav_color = "#c7a7a7";
+
+    onMount(() => {
+        fetch("https://icons.hackclub.com/api/icons/white/leaders")
+        .then(response => response.text())
+        .then(svg => {
+            icon = svg;
+            icon_load = true;
+        })
+    })
+
+    let name = "Badges";
 </script>
 
-<img id="flag" src={flag} alt="orpheus holding a flag with hack club written on it" />
+<a href = "https://hackclub.com/"><img id="flag" src={flag} alt="orpheus holding a flag with hack club written on it" /></a>
+<div id="navbar" style="background-color: {nav_color}">
+    <div id="logo" class:loaded={icon_load}>
+        {@html icon}
+    </div>
+</div>
