a7be72bbb73e2cc525f4322cea6aa94d3be9c409
lynn89-sudo <luke87joseph@gmail.com>
2025-06-26 01:58:59 +0000

    Init Sveltekit

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    let logo_src = "null"
+    import flag from "$lib/assets/orpheus.svg";
+</script>
+
+<img id="flag" src={flag} alt="orpheus holding a flag with hack club written on it" />
